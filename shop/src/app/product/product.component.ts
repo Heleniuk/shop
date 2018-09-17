d64e6b9a687dfd8df2a7e8162dff89e1ddd991ac
Heleniuk <elena.vizniuk@gmail.com>
2018-09-17 23:24:22 +0300

    Added a template for Product Component

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+  reviews: string[];
+  ratings: Array<Number> = new Array();
+
   constructor() { }
 
   ngOnInit() {
+    this.name = "A Book";
+    this.description = "A very interesting one, by the way";
+    this.price = 100;
+    this.category = Category.Business;
+    this.isAvailable = true;
+    this.reviews = ['Perfect!', 'Wonderful!'];
+    this.ratings.push(5);
+    this.ratings.push(5);
   }
 
+}
+
+const enum Category {
+  Mainstream, Business, Luxury
 }
